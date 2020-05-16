d5e1d5ce56b243fe0bbee9b0740ba41ff1e58078
mtnmosesmayimela <moses.mayimela@mtn.com>
2020-05-16 23:12:57 +0200

    initial commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: '1',
+    name: 'Superman Hero',
+    email: 'superman@hero.com',
+    status: 'active'
+  },
+  {
+    id: '2',
+    name: 'Thor Marvel',
+    email: 'thor@marvel.com',
+    status: 'inactive'
+  },
+  {
+    id: '3',
+    name: 'Neo Matrix',
+    email: 'neo@matrix.com',
+    status: 'active'
+  }
+];
+
+module.exports = members;
---
+++ routes/api/members.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const uuid = require('uuid');
+const router = express.Router();
+const members = require('../../Members');
+
+// Gets All Members
+router.get('/', (req, res) => res.json(members));
+
+// Get Single Member
+router.get('/:id', (req, res) => {
+  const found = members.some(member => member.id ==(req.params.id));
+
+  if (found) {
+    res.json(members.filter(member => member.id ==(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+// Create Member
+router.post('/', (req, res) => {
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: 'active'
+  };
+
+  if (!newMember.name || !newMember.email) {
+    return res.status(400).json({ msg: 'Please include a name and email' });
+  }
+
+  members.push(newMember);
+  res.json(members);
+  // res.redirect('/');
+});
+
+// Update Member
+router.put('/:id', (req, res) => {
+  const found = members.some(member => member.id ==(req.params.id));
+
+  if (found) {
+    const updMember = req.body;
+    members.forEach(member => {
+      if (member.id ==(req.params.id)) {
+        member.name = updMember.name ? updMember.name : member.name;
+        member.email = updMember.email ? updMember.email : member.email;
+
+        res.json({ msg: 'Member updated', member });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+// Delete Member
+router.delete('/:id', (req, res) => {
+  var removeIndex = members.map(function(item) { return item.id; }).indexOf(req.params.id);
+
+  if (removeIndex>-1) {
+    members.splice(removeIndex, 1);
+    res.json({
+      msg: 'Member deleted',
+      members: members
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+module.exports = router;

b5b9793bead897558a45ff3b707269b05ebb5d74
mtnmosesmayimela <moses.mayimela@mtn.com>
2020-05-17 08:06:52 +0200

    new world

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Homepage Route
 app.get('/', (req, res) =>
-  res.send("{\"Status\":\"happy\"}")
+  res.send("{\"Status\":\"new world!\"}")
 );
 
 // Set static folder

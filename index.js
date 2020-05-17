bcf9d50e040492be54e157b5994ceae23330e23e
mtnmosesmayimela <moses.mayimela@mtn.com>
2020-05-17 02:35:19 +0200

    Change message1

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Homepage Route
 app.get('/', (req, res) =>
-  res.send("{\"Status\":\"Concourse test\"}")
+  res.send("{\"Status\":\"Moses\"}")
 );
 
 // Set static folder
